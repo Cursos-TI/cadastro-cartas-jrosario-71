e600d32c9115999a2c08015caf9c559244d633c9
jrosario-71 <ldrosario2018@gmail.com>
2026-08-13 02:39:39 +0000

    Alterações de códigos cálculo de média

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
+// Calcular média de notas
+// Tema 1 - Cadastro das notas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int nota1, nota2, nota3;
+  int media;
+
+  printf("Programa de cálculo de media de notas: \n");
+  printf("digite sua primeira nota: \n");
+  scanf("%d", &nota1);
+  printf("digite sua segunda nota: \n");
+  scanf("%d", &nota2);
+  printf("digite sua terceira nota: \n");
+  scanf("%d", &nota3);
+
+  media =  (nota1 + nota2 + nota3) / 3;
+  printf("A media é: %d", media);
+
+
+
+
+
+  // Área para entrada de dados - Cadastro Carta 1
   
 
-  // Área para entrada de dados
-
   // Área para exibição dos dados da cidade
-  printf("Olá mundo, no Codespace\n");
-  printf("Meu nome é Lindonjohnson\n");
+ 
 
 return 0;
 } 
