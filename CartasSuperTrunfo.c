536085ddb9bcb75bc42db476b20680ab7a802ab7
jrosario-71 <ldrosario2018@gmail.com>
2026-08-16 03:01:22 +0000

    Adicionado novos códigos no super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,48 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+    // Declaracao das variavies
+
+    char produtoA[30] = "ProdutoA";
+    char produtoB[30] = "PrudutoA";
+
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 500;
+
+    float valorA = 10.50;
+    float valorB = 20.40;
+
+    unsigned estoqueMinimoA = 500;
+    unsigned estoqueMinimoB = 2.500;
+
+    double valorTotalA;
+    double valorTotalB;
+
+    int resultadoA, resultadoB;
+
+    // Exibir as inormacoes dos produtos
+
+    printf("Produto %s tem estoque %u e o valor unitario é R$ %.2f\n", produtoA, estoqueA, valorA);
+    printf("Produto %s tem estoque %u e o valor unitario é R$ %.2f\n", produtoB, estoqueB, valorB);
+
+    // Comparacoes com o valor minimo de estoque
+
+    resultadoA = estoqueA > estoqueMinimoA;
+    resultadoB = estoqueB > estoqueMinimoB;
+
+    printf("O produto %s tem estoque minimo %d\n", produtoA, resultadoA);
+    printf("O produto %s tem estoque minimo %d\n", produtoB, resultadoB);
+
+
+  int a = 10;
+  int b = 20;
+
+  printf("a > b: %d\n", a > b);
+  printf("a < b: %d\n", a < b);
+  printf("a == b: %d\n", a == b);
+  printf("a != b: %d\n", a != b);
+
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
   int nota1, nota2, nota3;
   int media;
@@ -18,7 +60,7 @@ int main() {
   scanf("%d", &nota3);
 
   media =  (nota1 + nota2 + nota3) / 3;
-  printf("A media é: %d", media);
+  printf("A media é: %d\n", media);
 
 
 
